e43203b5b75492cda49dcaeeef5beea835810bb2
Your Name <you@example.com>
2021-04-02 10:30:53 +0200

    update

--- project5-elk/terraform/modules/stg/keypair/main.tf
+++ project5-elk/terraform/modules/stg/keypair/main.tf
@@ -1,4 +1,4 @@
 resource "openstack_compute_keypair_v2" "project5-elk-keypair" {
   name       = var.pubkey_name
-  public_key = file("/root/github/openstack/project5-elk/secrets/project5-keypair.pub")
+  public_key = file("/home/marek/github/openstack/project5-elk/secrets/project5-keypair.pub")
 }
